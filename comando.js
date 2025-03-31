b27f06fdfa085c2667008f8a77ae249a2606ff7d
NICOLLAS JOSE PRIM <nicollas_prim@estudante.sesisenai.org.br>
2025-03-31 09:57:46 -0300

    Feito

---
+++ comando.js
@@ -0,0 +1,162 @@
+class Obj{
+   constructor(x,y,w,h,a){
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+    this.a = a
+
+   }
+
+    draw(){
+        let img = new Image()
+        img.src = this.a
+        des.drawImage(img, this.x, this. y, this.w, this.h) 
+    }
+}
+
+class Car extends Obj{
+    dir = 0
+    vida = 5
+    pontos = 0
+    invencivel = false
+    move(){
+    
+        if(this.x <= 0){
+            this.x = 0
+        }else if(this.x >= 500){
+            this.x = 500
+        }
+
+        this.x += this.dir
+    }
+    point(objeto) {
+        if ((objeto.y >= 680) && (objeto.y <= 684)) {
+            return true
+        } else {
+            false
+        }
+    }
+
+    colid(objeto) {
+        if (!this.invencivel && 
+            (this.x < objeto.x + objeto.w) &&
+            (this.x + this.w > objeto.x) &&
+            (this.y < objeto.y + objeto.h) &&
+            (this.y + this.h > objeto.y)) {
+            return true;
+        }
+        return false;
+    }
+
+    inPitStop = false
+    pitStopProgresso = 0
+    pitStopDuracao = 3000
+}
+
+class PitStop extends Car{
+    activate = true
+    move() {
+        this.y += speed
+        if (this.y >= 2000) {
+            this.recomeca()
+        }
+    }
+
+    recomeca() {
+        this.y = -1000;
+        this.x = Math.random() * (600 - this.w);
+        this.active = true; 
+    }
+}
+
+class Carro2 extends Obj {
+    constructor(x, y, w, h, a, speed) {
+        super(x, y, w, h, a);
+        this.speed = speed; 
+        this.justRespawned = false;
+    }
+    
+    move(){
+        if(this.y >= 1100){
+            this.y = -160;
+            this.x = Math.random() * 500;
+            this.justRespawned = true; 
+        }
+        this.y += this.speed;
+    }
+
+    recomeca() {
+        this.y = -110;
+        this.x = Math.random() * 458;
+        this.justRespawned = false; 
+    }
+}
+
+class Carro3 extends Obj {
+    constructor(x, y, w, h, a) {
+        super(x, y, w, h, a);
+        this.speed = 6; 
+        this.justRespawned = false;
+    }
+    
+    move(){
+        if(this.y >= 1100){
+            this.y = -160;
+            this.x = Math.random() * 500;
+            this.justRespawned = true; 
+        }
+        this.y += this.speed;
+    }
+
+    recomeca() {
+        this.y = -110;
+        this.x = Math.random() * 458;
+        this.justRespawned = false; 
+    }
+}
+
+class Carro4 extends Obj {
+    constructor(x, y, w, h, a) {
+        super(x, y, w, h, a);
+        this.speed = 5; 
+        this.justRespawned = false;
+    }
+    
+    move(){
+        if(this.y >= 1100){
+            this.y = -160;
+            this.x = Math.random() * 500;
+            this.justRespawned = true; 
+        }
+        this.y += this.speed;
+    }
+
+    recomeca() {
+        this.y = -110;
+        this.x = Math.random() * 458;
+        this.justRespawned = false; 
+    }
+}
+class Estrada extends Obj {
+    des_estrada() {
+        des.fillStyle = this.a
+        des.fillRect(this.x, this.y, this.w, this.h)
+    }
+
+    mov_est() {
+        this.y += 8
+        if (this.y >= 900) {
+            this.y = 10
+        }
+    }
+}
+
+class Text {
+    des_text(text, x, y, cor, font) {
+        des.fillStyle = cor
+        des.lineWidht = '5'
+        des.font = font
+        des.fillText(text, x, y)
+    }
+}
---
+++ index.js
@@ -0,0 +1,378 @@
+
+let des = document.getElementById('des')?.getContext('2d')
+    
+const car = new Obj(0, 0, 600, 900, './img/menuGame.png')
+const f1 = new Car(300, 650, 100, 150, './img/carro3Direita.png')
+const c2 = new Carro2(100, -180, 90, 150, './img/carro2Esquerda.png', 8)
+const c3 = new Carro3(300, -180, 90, 150, './img/renan-removebg-preview.png')
+const c4 = new Carro4(500, -180, 90, 150, './img/mcLata-removebg-preview.png')
+
+let pitStop = new PitStop(600, -40, 100, 100, './img/pitStop.png')
+let zebra1 = new Estrada(560, 0, 55, 90, 'red')
+let zebra2 = new Estrada(560, 90, 50, 90, 'white')
+let zebra3 = new Estrada(560, 180, 55, 90, 'red')
+let zebra4 = new Estrada(560, 270, 55, 90, 'white')
+let zebra5 = new Estrada(560, 360, 55, 90, 'red')
+let zebra6 = new Estrada(560, 450, 55, 90, 'white')
+let zebra7 = new Estrada(560, 540, 55, 90, 'red')
+let zebra8 = new Estrada(560, 630, 55, 90, 'white')
+let zebra9 = new Estrada(560, 720, 55, 90, 'red')
+let zebra10 = new Estrada(560, 810, 55, 90, 'red')
+let zebra11 = new Estrada(560, 900, 55, 90, 'white')
+let zebra12 = new Estrada(560, 990, 55, 90, 'red')
+let zebra1e = new Estrada(0, 0, 50, 90, 'red')
+let zebra2e = new Estrada(0, 90, 50, 90, 'white')
+let zebra3e = new Estrada(0, 180, 50, 90, 'red')
+let zebra4e = new Estrada(0, 270, 50, 90, 'white')
+let zebra5e = new Estrada(0, 360, 50, 90, 'red')
+let zebra6e = new Estrada(0, 450, 50, 90, 'white')
+let zebra7e = new Estrada(0, 540, 50, 90, 'red')
+let zebra8e = new Estrada(0, 630, 50, 90, 'white')
+let zebra9e = new Estrada(0, 720, 50, 90, 'red')
+let zebra10e = new Estrada(0, 810, 50, 90, 'red')
+let zebra11e = new Estrada(0, 900, 50, 90, 'white')
+let zebra12e = new Estrada(0, 990, 50 , 90, 'red')
+
+let textFase = new Text()
+
+let pitStopPaused = false
+let pitStopStartTime = 0
+pitStop.speed = 5
+
+let speed;
+
+let game = true
+
+let sair = document.getElementById('sair')
+
+//let menuSong = new Audio('./assets/gaem oveh.wav')
+//let gameSong = new Audio('./assets/gaem oveh.wav')
+//let batida = new Audio('./assets/rocketlaucnher.mp3')
+//let pointSound = new Audio('./assets/rocketlaucnher.mp3')
+
+let fase = 1
+let phaseMessage = ''
+let phaseMessageTime = 0
+
+let gameOver = false
+
+//menuSong.volume = 1
+//gameSong.volume = 1
+//menuSong.loop = true
+//gameSong.loop = true
+//batida.volume = 0.3
+
+function reiniciarJogo() {
+    iniciarJogo()
+    gameOver = false
+}
+
+function iniciarJogo() {
+    gameOver = false
+    f1.vida = 3
+    f1.pontos = 0
+    f1.x = 100
+    f1.y = 650
+    c2.recomeca()
+    c3.recomeca()
+    c4.recomeca()
+    pitStop.recomeca()
+    game = true
+}
+
+function voltaMenu(){
+    if(gameOver){
+        
+    }
+}
+
+document.addEventListener('keypress', (e) => {
+    if (gameOver) {
+        if (e.key.toLowerCase() === 'g') reiniciarJogo()
+        if (e.key.toLowerCase() === 'p') voltaMenu() // Adicionado
+    } else {
+        if (e.key.toLowerCase() === 'g' && !game) iniciarJogo()
+        if (e.key.toLowerCase() === 'p') game = false
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if (game && !pitStopPaused) { 
+        if (e.key === 'a') {
+            f1.dir = -10
+        } else if (e.key === 'd') {
+            f1.dir = +10
+        }
+    }
+})
+
+document.addEventListener('keyup', (e) => {
+    if (game) {
+        if (e.key === 'a' || e.key === 'd') {
+            f1.dir = 0
+        }
+    }
+})
+
+function voltaMenu(){
+    game = false
+    gameOver = false
+    f1.vida = 5
+    f1.pontos = 0
+    f1.x = 100
+    f1.y = 650
+    c2.recomeca()
+    c3.recomeca()
+    c4.recomeca()
+    pitStop.recomeca()
+    
+    des.clearRect(0, 0, 600, 900)
+    desenharInicio()
+}
+
+
+
+function colisao() {
+    if (f1.colid(c2)) {
+        f1.vida -= 1;
+        f1.invencivel = true 
+        c2.recomeca()
+        setTimeout(() => {
+            f1.invencivel = false
+        }, 1000)
+    }
+    if(f1.colid(c3)){
+        f1.vida -= 1
+        f1.invencivel = true
+        c3.recomeca()
+        setTimeout(() => {
+            f1.invencivel = false
+        }, 1000)
+    }
+    if(f1.colid(c4)){
+        f1.vida -= 1
+        f1.invencivel = true
+        c4.recomeca()
+        setTimeout(()=>{
+            f1.invencivel = false
+        }, 1000)
+    }
+    if (f1.colid(pitStop)) {
+        if(f1.vida < 5) { 
+            f1.vida += 1;
+        }
+        pitStop.recomeca()
+    }
+}
+
+function pontos(){
+    if (f1.point(c2)) {
+        f1.pontos += 1
+    }
+    if(f1.point(c3)){
+        f1.pontos += 1
+    }
+    if(f1.point(c4)){
+        f1.pontos += 1
+    }
+}
+
+
+function atualizar(){
+    zebra1.mov_est()
+    zebra2.mov_est()
+    zebra3.mov_est()
+    zebra4.mov_est()
+    zebra5.mov_est()
+    zebra6.mov_est()
+    zebra7.mov_est()
+    zebra8.mov_est()
+    zebra9.mov_est()
+    zebra10.mov_est()
+    zebra11.mov_est()
+    zebra12.mov_est()
+    zebra1e.mov_est()
+    zebra2e.mov_est()
+    zebra3e.mov_est()
+    zebra4e.mov_est()
+    zebra5e.mov_est()
+    zebra6e.mov_est()
+    zebra7e.mov_est()
+    zebra8e.mov_est()
+    zebra9e.mov_est()
+    zebra10e.mov_est()
+    zebra11e.mov_est()
+    zebra12e.mov_est()
+    f1.move()
+    c2.move()
+    c3.move()
+    c4.move()
+
+    pitStop.move()
+    if(c2.justRespawned) {
+        f1.pontos += 1
+        c2.justRespawned = false
+    }
+    colisao()
+    if(f1.colid(c2)) {
+        f1.vida -= 1
+        c2.recomeca()
+        f1.x = 100 
+        f1.y = 600
+    }
+    if(c3.justRespawned) {
+        f1.pontos += 1
+        c3.justRespawned = false
+    }
+    colisao()
+    if(f1.colid(c3)) {
+        f1.vida -= 1
+        c3.recomeca()
+        f1.x = 100  
+        f1.y = 600
+    }
+    if(c4.justRespawned) {
+        f1.pontos += 1
+        c4.justRespawned = false
+    }
+    colisao()
+    if(f1.colid(c4)) {
+        f1.vida -= 1
+        c4.recomeca()
+        f1.x = 100;
+        f1.y = 600
+    }
+    if(f1.vida <= 0) {
+        game = false
+        gameOver = true;
+    }
+    if(pitStop.y >= 900) {
+        pitStop.recomeca()
+    }
+}
+
+function desenhar(){
+    des.fillStyle = 'grey'; 
+    des.fillRect(0, 0, 600, 900)
+    
+    zebra1.des_estrada()
+    zebra2.des_estrada()
+    zebra3.des_estrada()
+    zebra4.des_estrada()
+    zebra5.des_estrada()
+    zebra6.des_estrada()
+    zebra7.des_estrada()
+    zebra8.des_estrada()
+    zebra9.des_estrada()
+    zebra10.des_estrada()
+    zebra11.des_estrada()
+    zebra12.des_estrada()
+    zebra1e.des_estrada()
+    zebra2e.des_estrada()
+    zebra3e.des_estrada()
+    zebra4e.des_estrada()
+    zebra5e.des_estrada()
+    zebra6e.des_estrada()
+    zebra7e.des_estrada()
+    zebra8e.des_estrada()
+    zebra9e.des_estrada()
+    zebra10e.des_estrada()
+    zebra11e.des_estrada()
+    zebra12e.des_estrada()
+    f1.draw();
+    c2.draw();
+    c3.draw();
+    c4.draw();
+
+    if(f1.pontos == 0) {
+        textFase.des_text('Gp da AUSTRALIA!', 120, 200, 'white',"35px Bankgothic md bt", )
+    }
+    if(f1.pontos == 20) {
+        textFase.des_text('Gp da CHINA!', 120, 200, 'white',"35px Bankgothic md bt")
+    }
+    if(f1.pontos >= 20 && f1.pontos < 40) {
+        c2.speed * c2.speed * 1.5
+        c3.speed * c2.speed * 1.5
+        c4.speed * c2.speed * 1.5
+    }
+    if(f1.pontos == 40) {
+        textFase.des_text('Gp de MÔNACO!', 120, 200, 'white',"35px Bankgothic md bt")
+    }
+    if(f1.pontos >= 40 && f1.pontos < 60) {
+        c2.speed * c2.speed * 2
+        c3.speed * c2.speed * 2 
+        c4.speed * c2.speed * 2 
+    }
+    if(f1.pontos == 60) {
+        textFase.des_text('Gp de INTERLAGOS!', 120, 200, 'white',"35px Bankgothic md bt")
+    }
+    if(f1.pontos >= 60 && f1.pontos < 80) {
+        c2.speed * c2.speed * 3
+        c3.speed * c2.speed * 3 
+        c4.speed = c2.speed * 3
+    }
+
+    pitStop.draw();
+    
+    des.fillStyle = 'white';
+    des.font = '20px Bankgothic md bt';
+    des.fillText(`Vidas: ${f1.vida}`, 30, 30);
+    des.fillText(`Ultrapassagens: ${f1.pontos}`, 30, 60);
+    
+    if(f1.inPitStop) {
+        des.fillStyle = 'rgba(0, 0, 0, 0.7)';
+        des.fillRect(0, 0, 600, 900);
+        
+        const progresso = f1.pitStopProgress / f1.pitStopDuration;
+        des.fillStyle = 'yellow';
+        des.fillRect(150, 400, 300 * progresso, 30);
+        
+        des.fillStyle = 'white';
+        des.font = '30px Arial';
+        des.fillText('PIT STOP', 250, 380);
+        des.fillText(`${Math.floor(progresso * 100)}%`, 280, 450);
+    }
+    
+    if(phaseMessageTime > 0){
+        des.fillStyle = 'gold';
+        des.font = '40px Bankgothic md bt';
+        des.textAlign = 'center';
+        des.fillText(phaseMessage, 300, 450);
+        phaseMessageTime -= 16;
+    }
+}
+
+function desenharInicio(){
+    car.draw()
+}
+
+function desenharGameOver() {
+    des.fillStyle = 'red';
+    des.font = '60px futura md bt';
+    des.textAlign = 'center';
+    des.fillText('GAME OVER', 300, 450);
+    
+    des.fillStyle = 'white';
+    des.font = '20px Arial';
+    des.fillText('Pressione G para recomeçar ou P para voltar ao menu!', 300, 500);
+}
+
+function main(){
+    if(game) {
+        des.clearRect(0, 0, 600, 900); 
+        atualizar();
+        desenhar();
+        requestAnimationFrame(main);
+    } else {
+        des.clearRect(0, 0, 600, 900);
+        if(gameOver) {
+            desenharGameOver();
+        } else {
+            desenharInicio();
+        }
+        requestAnimationFrame(main);
+    }
+}
+
+main();
+ 
